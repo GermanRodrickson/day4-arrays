edc26f6c4c4f2d16aece4ba507f99268be795cc5
GermanRodrickson <germanrodrickson@gmail.com>
2019-01-06 08:28:25 +0100

    map and sort

--- index.js
+++ index.js
@@ -45,8 +45,20 @@ const years = inventors.reduce((total, inventor) => {
   return total + (inventor.passed - inventor.year);
 }, 0);
 console.log(years);
-
 // 5. Sort the inventors by years lived
+const yearsLived = inventors.map(inventors => {
+  return (inventors.passed - inventors.year)
+}).sort((a, b) => {
+  if (a.year > b.year) {
+    return 1;
+  }
+  if (a.year < b.year) {
+    return -1;
+  }
+  return 0;
+});
+console.table(yearsLived);
+
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 // 7. sort Exercise
